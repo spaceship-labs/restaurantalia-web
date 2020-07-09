5ccd5470df9891ab8e299779f385108af3db566c
Sergio Can <gersiopumas@gmail.com>
2020-07-09 17:59:09 -0500

    Category order and pseudo balancer

--- src/utils/format.js
+++ src/utils/format.js
@@ -12,7 +12,36 @@ function formatTitle({ nombre }) {
   let lastChar = nombre.slice(-1);
   return lastChar == '.' ? nombre : nombre + '.';
 }
+
+function categoryBalancer(categories, numColumns = 3) {
+  const AllCategories = Object.keys(categories)
+  if (AllCategories.length < numColumns) {
+    let Initial = numColumns == 2 ? [[], []] : [[], [], []]
+    return Object.keys(categories).reduce(
+      (cols, cat, i) => {
+        const c = i % numColumns;
+        cols[c].push(categories[cat]);
+        return cols;
+      },
+      Initial
+    );
+  }
+  let InitialValue = []
+  let j = AllCategories.length;
+  let chunk = AllCategories.length / numColumns;
+  let index = 0
+  for (let i = 0; i < j; i += chunk) {
+    InitialValue.push([])
+    let temparray = AllCategories.slice(i, i + chunk);
+    temparray.forEach((value) => {
+      InitialValue[index].push(categories[value])
+    })
+    index++;
+  }
+  return InitialValue;
+}
 export {
   formatDescription,
-  formatTitle
+  formatTitle,
+  categoryBalancer
 }
