f6320417ddee1eacf4a59e80a125c3d74ef96c3e
Sergio Can <gersiopumas@gmail.com>
2020-07-13 17:40:59 -0500

    Added Category Balancer

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,3 +1,4 @@
+import { getDishes } from './api'
 function formatDescription({ descripcion }) {
   if (!descripcion) {
     return descripcion;
@@ -13,9 +14,44 @@ function formatTitle({ nombre }) {
   return lastChar == '.' ? nombre : nombre + '.';
 }
 
-function categoryBalancer(categories, numColumns = 3) {
+async function getBalancedColumns(categories, numColumns = 3) {
+  return await Promise.all(categories.map(async category => {
+    return (await getDishes(category)).length || 0
+  })).then(dishesByCategory => {
+    let totalOfDishes = dishesByCategory.reduce((acc, dishes) => acc + dishes, 0);
+    let size = Math.floor(totalOfDishes / numColumns);
+    let threshold = 10; // 5 dishes as threshold
+    let Column = {
+      sumatory: 0,
+      index: -1,
+      Categories: []
+    }
+    let Columns = []
+    for (let i = 0; i < numColumns; i++) {
+      let InitialValue = {
+        sumatory: 0,
+        index: Column.index || -1,
+        Categories: []
+      }
+      Column = dishesByCategory.reduce((acc, value, index) => {
+        if (index > acc.index) {
+          acc.sumatory += value
+          if (acc.sumatory < size + threshold || i == numColumns - 1) {
+            acc.Categories.push(index)
+            acc.index = index;
+          }
+        }
+        return acc;
+      }, InitialValue);
+      Columns.push(Column.Categories)
+    }
+    return Columns;
+  })
+}
+async function categoryBalancer(categories, numColumns = 3) {
+  // in case categories doesnt fill all use normal balancer.
   const AllCategories = Object.keys(categories)
-  if (AllCategories.length < numColumns) {
+  if (AllCategories.length <= numColumns) {
     let Initial = numColumns == 2 ? [[], []] : [[], [], []]
     return Object.keys(categories).reduce(
       (cols, cat, i) => {
@@ -26,19 +62,27 @@ function categoryBalancer(categories, numColumns = 3) {
       Initial
     );
   }
-  let InitialValue = []
-  let j = AllCategories.length;
-  let chunk = AllCategories.length / numColumns;
-  let index = 0
-  for (let i = 0; i < j; i += chunk) {
-    InitialValue.push([])
-    let temparray = AllCategories.slice(i, i + chunk);
-    temparray.forEach((value) => {
-      InitialValue[index].push(categories[value])
-    })
-    index++;
-  }
-  return InitialValue;
+  // use custom balancer instead
+  let BalancedColumns = await getBalancedColumns(categories, numColumns)
+  const Columns = BalancedColumns.map(Column => {
+    return Column.map(value => categories[value]);
+  })
+  return Columns;
+
+  //let InitialValue = []
+  //let j = AllCategories.length;
+  //let chunk = AllCategories.length / numColumns;
+  //let index = 0
+  //for (let i = 0; i < j; i += chunk) {
+  //  InitialValue.push([])
+  //  let temparray = AllCategories.slice(i, i + chunk);
+  //  temparray.forEach((value) => {
+  //    InitialValue[index].push(categories[value])
+  //  })
+  //  index++;
+  //}
+  //console.log({ InitialValue })
+  //return InitialValue;
 }
 export {
   formatDescription,
